99fac3b1063a63c46323e21d7641364e8f248ded
Mcflaber <danielaramirez4949@gmail.com>
2026-03-30 11:15:22 -0500

    save

--- Assets/lab06/CollisionTools.cs
+++ Assets/lab06/CollisionTools.cs
@@ -47,12 +47,25 @@ public static class CollisionTools
 
     public static bool IsPointInRectangle(Vector3 Point, Rect Box)
     {
-        // stub code 
-        return false; 
+        return Point.x >= Box.X && Point.x <= Box.X + Box.Width && Point.y >= Box.Y && Point.y <= Box.Y + Box.Height;
     }
     public static bool IsPointInTriangle(Vector3 Point, TriangleData Triangle)
     {
-        // stub code 
-        return false;
+
+        Vector3 v0 = Triangle.PointC - Triangle.PointA;
+        Vector3 v1 = Triangle.PointB - Triangle.PointA;
+        Vector3 v2 = Point - Triangle.PointA;
+
+        float dot00 = Vector3.Dot(v0, v0);
+        float dot01 = Vector3.Dot(v0, v1);
+        float dot02 = Vector3.Dot(v0, v2);
+        float dot11 = Vector3.Dot(v1, v1);
+        float dot12 = Vector3.Dot(v1, v2);
+
+        float invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+        float u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+        float v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+        return (u >= 0) && (v >= 0) && (u + v < 1);
     }
 }
